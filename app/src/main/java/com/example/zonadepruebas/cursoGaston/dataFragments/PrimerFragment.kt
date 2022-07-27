bfd8353269d61a903b7fe2fdf346de6229828f74
Eduardo Ibacache <eduardoibacache@MacBook-Air-de-Eduardo.local>
2022-07-26 23:39:39 -0400

    :rocket: Se realiza primera funcionalidad

--- app/src/main/java/com/example/zonadepruebas/cursoGaston/dataFragments/PrimerFragment.kt
+++ app/src/main/java/com/example/zonadepruebas/cursoGaston/dataFragments/PrimerFragment.kt
@@ -6,19 +6,31 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.TextView
 import androidx.fragment.app.commit
+import androidx.fragment.app.setFragmentResult
+import androidx.fragment.app.setFragmentResultListener
 import com.example.zonadepruebas.R
 
 class PrimerFragment : Fragment(R.layout.fragment_primer) {
 
+    private lateinit var tv :TextView
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         val button = view.findViewById<Button>(R.id.btnirasegundo)
+
+        tv = view.findViewById(R.id.tvUno)
+
         button.setOnClickListener {
             requireActivity().supportFragmentManager.commit {
                 replace(R.id.fcv_container, SegundoFragment.newInstance("Eduardo", "1234"))
                 addToBackStack("primerFragment")
             }
+        }
+
+        setFragmentResultListener("keyResult"){
+            key, bundle ->
+            tv.text = bundle.getString("segundon")
         }
 
 
--- app/src/main/java/com/example/zonadepruebas/cursoGaston/dataFragments/SegundoFragment.kt
+++ app/src/main/java/com/example/zonadepruebas/cursoGaston/dataFragments/SegundoFragment.kt
@@ -5,8 +5,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.TextView
 import androidx.core.os.bundleOf
+import androidx.fragment.app.setFragmentResult
 import com.example.zonadepruebas.R
 
 class SegundoFragment : Fragment(R.layout.fragment_segundo) {
@@ -23,7 +25,13 @@ class SegundoFragment : Fragment(R.layout.fragment_segundo) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         val tvFUno = view.findViewById<TextView>(R.id.tvDeFragmentUno)
+        val btn = view.findViewById<Button>(R.id.btnVolverMsg)
         tvFUno.text = "Nombre: $nombre \nRut: $rut"
+
+        btn.setOnClickListener {
+            setFragmentResult("keyResult", bundleOf("segundon" to "Hola amigos soy segundo fragment"))
+            requireActivity().supportFragmentManager.popBackStack()
+        }
 
     }
 
